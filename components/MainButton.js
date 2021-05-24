d5860ea87520baf440592ac02197959300f2b21d
Bouloute <annaick1995@gmail.com>
2021-05-24 14:43:47 -0400

    first commit

---
+++ components/MainButton.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { StyleSheet, Image, Text, View } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 66,
+    height: 58,
+  },
+});
+
+export default class MainButton extends Component {
+    render() {
+        return (
+            <View>
+            <Image 
+                source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
+                style={styles.tinyLogo}
+            />
+                <Text> Hellko its me Bip o </Text>
+            </View>
+        )
+    }
+}
