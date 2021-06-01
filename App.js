f11e675266e08e6efe04e5f2cc04e84a3691f26e
Bouloute <annaick1995@gmail.com>
2021-05-31 21:59:31 -0400

    adding button to the header

--- App.js
+++ App.js
@@ -1,11 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import type {Node} from 'react';
 import {
@@ -14,19 +6,11 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  Button,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-//import { StackNavigator,} from 'react-navigation';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -36,32 +20,52 @@ import NavBar from './components/NavBar'
 import HomeScreen from './screens/HomeScreen'
 import AnotherScreen from './screens/AnotherScreen'
 
-/*
-const App = StackNavigator({
-  Home: { screen: HomeScreen },
-  //Profile: { screen: ProfileScreen },
-});
-*/
 const Stack = createStackNavigator();
-
-/*
-const App: () => Node = () => {
-  return (
-    <View>
-      <NavBar/>
-      <Text>hello im bipio</Text>
-    </View>
-  );
-};
-*/
 
 function App() {
   return (
+    
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Another" component={AnotherScreen} />
-
+      <Stack.Navigator
+        screenOptions={{
+          headerTintColor: 'white',
+          headerStyle: { backgroundColor: 'grey' },
+        }}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen}  
+          options={{
+            headerRight: () => (
+              <Text
+                onPress={() => alert('This is a WIP!')}
+                style={{ 
+                  fontSize: 20,
+                  color: 'white',
+                  marginRight: 20
+                }}
+              > Login
+              </Text>
+            ),
+          }} 
+        />
+        <Stack.Screen 
+          name="Another" 
+          component={AnotherScreen}
+          options={{
+          headerRight: () => (
+            <Text
+              onPress={() => alert('This is a WIP!')}
+              style={{ 
+                fontSize: 20,
+                color: 'white',
+                marginRight: 20
+              }}
+            > Login
+            </Text>
+          ),
+          }} 
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
