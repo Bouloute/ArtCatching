5cf8248ea5d48d15858a288625959553fa0e8896
Bouloute <annaick1995@gmail.com>
2021-06-13 21:00:51 -0400

    4 buttons on main screen in pattern

--- components/MainButton.js
+++ components/MainButton.js
@@ -6,8 +6,9 @@ const styles = StyleSheet.create({
     paddingTop: 50,
   },
   tinyLogo: {
-    width: 50,
-    height: 50,
+    width: 150,
+    height: 150,
+    margin: 30
   },
   logo: {
     width: 66,
@@ -19,11 +20,11 @@ export default class MainButton extends Component {
     render() {
         return (
             <View>
-            <Image 
-                source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
-                style={styles.tinyLogo}
-            />
-                <Text> Hellko its me Bip o </Text>
+              <Image 
+                  source={{uri: 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAADMAAAAzCAYAAAA6oTAqAAAAEXRFWHRTb2Z0d2FyZQBwbmdjcnVzaEB1SfMAAABQSURBVGje7dSxCQBACARB+2/ab8BEeQNhFi6WSYzYLYudDQYGBgYGBgYGBgYGBgYGBgZmcvDqYGBgmhivGQYGBgYGBgYGBgYGBgYGBgbmQw+P/eMrC5UTVAAAAABJRU5ErkJggg=='}}
+                  style={styles.tinyLogo}
+              />
+                <Text style={{ justifyContent: 'center'}}> Hello its me Bipo </Text>
             </View>
         )
     }
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -3,15 +3,24 @@ import { Text, View, Button } from 'react-native'
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
+import MainButton from '../components/MainButton'
 
 export default function HomeScreen({ navigation })  {
     
-        return (
-            <View>
-                <Text> This is the home page! </Text>
-                <Button title="Another Page" onPress={() => {navigation.navigate('Another')}}></Button>
+    return (
+        <View>
+            <Text> This is the home page! </Text>
+            <Button title="Another Page" onPress={() => {navigation.navigate('Another')}}></Button>
+            <View style={{flexDirection: "row", justifyContent: 'center',}}>
+                <MainButton />
+                <MainButton />
             </View>
-        )
+            <View style={{flexDirection: "row", justifyContent: 'center',}}>
+                <MainButton />
+                <MainButton />
+            </View>
+            
+        </View>
+    )
     
 }
