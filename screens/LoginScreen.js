c8d002b63d841d152831865c9f96c0daff402fc4
Bouloute <annaick1995@gmail.com>
2021-07-08 08:51:12 -0400

    database maybe, need to fix nav

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,97 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Alert, Button, Text, TextInput, View } from 'react-native'
+import SQLite from 'react-native-sqlite-storage'
 
-export default class LoginScreen extends Component {
-    render() {
-        return (
-            <View>
-                <Text> Login qwertyui </Text>
-            </View>
-        )
+const db = SQLite.openDatabase(
+    {
+        name: 'MainDB',
+        location: 'default',
+    },
+    () => {},
+    error => {console.log(error)}
+)
+
+export default function LoginScreen({ navigation }) {
+    
+    const [name, setName] = useState('')
+    const [age, setAge] = useState('')
+
+    useEffect(() => {
+        createTable();
+        getData();
+    }, []);
+
+    const createTable = () => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                "CREATE TABLE IF NOT EXISTS "
+                + "Users "
+                + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT, Age INTEGER);"
+            )
+        })
     }
+    
+    const getData = () => {
+        try {
+            db.transaction((tx) => {
+                tx.executeSql(
+                    "SELECT Name, Age FROM Users",
+                    [],
+                    (tx, results) => {
+                        var len = results.rows.length;
+                        if (len > 0) {
+                            navigation.navigate('Home');
+                        }
+                    }
+                )
+            })
+        } catch (error) {
+            Alert.alert('Warning!', error)
+        }
+    }
+
+    const setData = async () => {
+
+        if (name.length == 0 || age.length == 0) {
+            Alert.alert('Warning!', 'Please write your data.')
+        } else {
+
+           try {
+               /* await db.transaction(async (tx) => {
+                    await tx.executeSql(
+                        "INSERT INTO Users (Name, Age) VALUES (?,?)",
+                        [name, age]
+                    );
+                })*/
+               // navigation.navigate('Home');
+                Alert.alert('Warning!', 'Got this far')
+                navigation.navigate('Home');
+            } catch (error) {
+                Alert.alert('Warning!', error.message)
+
+                console.log(error);
+            }
+        }
+        
+    }
+
+    return (
+        <View>
+            <Text> Login </Text>
+            <TextInput
+                placeholder='Enter your name'
+                onChangeText={(value) => setName(value)}
+            />
+            <TextInput
+                placeholder='Enter your age'
+                onChangeText={(value) => setAge(value)}
+            />
+            <Button
+                title='Login'
+                onPress={setData}
+            />
+            <Text>{name}</Text>
+            <Text>{age}</Text>
+        </View>
+    )
 }
